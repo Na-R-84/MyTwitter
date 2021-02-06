908a1155c4451ba149496a1fb282fab8dc6cf825
Basir <basir.jafarzadeh@gmail.com>
2021-02-06 17:50:47 +0330

    create basic server

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+
+const app = express();
+cont tweets = [];
+
+// MongoDB
+
+app.get('/api/users', (req, res) => {
+  res.send([
+    {
+      name: 'admin',
+      email: 'admin@example.com',
+      password: '123',
+      isAdmin: true,
+    },
+    {
+      name: 'user',
+      email: 'user@example.com',
+      password: '123',
+      isAdmin: false,
+    },
+  ]);
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log('server is ready at http://localhost:5000');
+});
